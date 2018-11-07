dd3d1575773b3b711c75483f18c7fd23cf2d652d
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2018-11-07 16:58:58 -0700

    removing redundancy

--- gdaljson/translate.py
+++ gdaljson/translate.py
@@ -9,6 +9,7 @@ def translate(input_vrt, bandList=None, srcWin=None, projWin=None, height=None, 
     """JSON encoded wrapper of gdal.Translate"""
 
     workingvrt = gdaljson.Raster(copy.deepcopy(input_vrt))
+    source = workingvrt.source
     gt = copy.deepcopy(workingvrt.gt)
 
     #Handle bands first
@@ -40,22 +41,22 @@ def translate(input_vrt, bandList=None, srcWin=None, projWin=None, height=None, 
         else:
             if height:
                 #If just height is given, calculate matching width.
-                ratio = workingvrt.data['VRTDataset']['VRTRasterBand'][0][workingvrt.source]['SrcRect']['@ySize'] / height
-                _width = int(workingvrt.data['VRTDataset']['VRTRasterBand'][0][workingvrt.source]['SrcRect']['@xSize']/ratio)
+                ratio = workingvrt.data['VRTDataset']['VRTRasterBand'][0][source]['SrcRect']['@ySize'] / height
+                _width = int(workingvrt.data['VRTDataset']['VRTRasterBand'][0][source]['SrcRect']['@xSize'] / ratio)
                 _height = height
             elif width:
                 #If just width is given, calculate matching height
-                ratio = workingvrt.data['VRTDataset']['VRTRasterBand'][0][workingvrt.source]['SrcRect']['@xSize'] / width
-                _height = int(workingvrt.data['VRTDataset']['VRTRasterBand'][0][workingvrt.source]['SrcRect']['@ySize']/ratio)
+                ratio = workingvrt.data['VRTDataset']['VRTRasterBand'][0][source]['SrcRect']['@xSize'] / width
+                _height = int(workingvrt.data['VRTDataset']['VRTRasterBand'][0][source]['SrcRect']['@ySize'] / ratio)
                 _width = width
             workingvrt.update_dst_rect([0,0,_width,_height])
         #Calculate new resolution based on new dimensions
-        gt[1] = (workingvrt.xres * workingvrt.data['VRTDataset']['VRTRasterBand'][0][workingvrt.source]['SrcRect']['@xSize']) / _width
-        gt[5] = -(workingvrt.yres * workingvrt.data['VRTDataset']['VRTRasterBand'][0][workingvrt.source]['SrcRect']['@ySize']) / _height
+        gt[1] = (workingvrt.xres * workingvrt.data['VRTDataset']['VRTRasterBand'][0][source]['SrcRect']['@xSize']) / _width
+        gt[5] = -(workingvrt.yres * workingvrt.data['VRTDataset']['VRTRasterBand'][0][source]['SrcRect']['@ySize']) / _height
 
     elif xRes and yRes:
-        ratiox = workingvrt.xres * workingvrt.data['VRTDataset']['VRTRasterBand'][0][workingvrt.source]['SrcRect']['@xSize']
-        ratioy = workingvrt.yres * workingvrt.data['VRTDataset']['VRTRasterBand'][0][workingvrt.source]['SrcRect']['@ySize']
+        ratiox = workingvrt.xres * workingvrt.data['VRTDataset']['VRTRasterBand'][0][source]['SrcRect']['@xSize']
+        ratioy = workingvrt.yres * workingvrt.data['VRTDataset']['VRTRasterBand'][0][source]['SrcRect']['@ySize']
         #Calculate new width and height based on input resolution
         _width = int(ratiox / xRes)
         _height = int(ratioy / yRes)
@@ -64,8 +65,8 @@ def translate(input_vrt, bandList=None, srcWin=None, projWin=None, height=None, 
         gt[5] = -yRes
 
     #Fix geotransform and image dimensions based on changes to DstRect
-    gt[0] = workingvrt.data['VRTDataset']['VRTRasterBand'][0][workingvrt.source]['SrcRect']['@xOff'] * workingvrt.xres + workingvrt.tlx
-    gt[3] = workingvrt.tly - workingvrt.data['VRTDataset']['VRTRasterBand'][0][workingvrt.source]['SrcRect']['@yOff'] * workingvrt.yres
+    gt[0] = workingvrt.data['VRTDataset']['VRTRasterBand'][0][source]['SrcRect']['@xOff'] * workingvrt.xres + workingvrt.tlx
+    gt[3] = workingvrt.tly - workingvrt.data['VRTDataset']['VRTRasterBand'][0][source]['SrcRect']['@yOff'] * workingvrt.yres
     workingvrt.data['VRTDataset']['GeoTransform']['$'] = ','.join([str(x) for x in gt])
 
     #Update raster size to match bands
