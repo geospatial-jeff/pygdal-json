6bc0edfd7cade25be20a66e5166eff46061e9302
geospatial-jeff <jeff.albrecht@slingshotaerospace.com>
2018-11-08 17:49:23 -0700

    Removing gdal dependency from projection handling

--- gdaljson/projection.py
+++ gdaljson/projection.py
@@ -1,53 +1,20 @@
-from osgeo import osr
+import json
+import os
+
+wkt = json.loads(open(os.path.join(os.path.split(os.path.realpath(__file__))[0], 'projections/gdal_epsg.json')).read())
 
 class SpatialRef():
 
-    @staticmethod
-    def _define_projection(wkt_string):
-        srs = osr.SpatialReference()
-        srs.ImportFromWkt(wkt_string)
-        return srs
-
     def __init__(self, wkt_string):
-        self.srs = self._define_projection(wkt_string)
-
-    @property
-    def epsg(self):
-        if self.srs.IsProjected():
-            return int(self.srs.GetAttrValue('AUTHORITY', 1))
-        elif self.srs.IsGeographic():
-            epsg_code = self.srs.GetAttrValue('AUTHORITY', 1)
-            proj_string = self.srs.ExportToWkt()
-            if not epsg_code:
-                if 'WGS_84' in proj_string:
-                    return 4326
-                elif 'North_American_1983' in proj_string:
-                    return 4269
-                else:
-                    print("WARNING: EPSG CODE NOT RECOGNIZED")
-            return int(epsg_code)
+        self.epsg = wkt_string.split(",")[-1][1:-3]
+        self.wkt = wkt[self.epsg][0]
 
     @property
     def linearunit(self):
-        return self.srs.GetLinearUnitsName()
+        return self.wkt.split("UNIT")[-1].split('"')[1]
 
     @property
     def is_geographic(self):
-        return self.srs.IsGeographic()
-
-    @property
-    def is_projected(self):
-        return self.srs.IsProjected()
-
-    def IsSame(self, srs):
-        return self.srs.IsSame(srs.srs)
-
-    def ExportToWkt(self):
-        return self.srs.ExportToWkt()
-
-    def ExportToXML(self):
-        return self.srs.ExportToXML()
-
-    def ExportToProj4(self):
-        return self.srs.ExportToProj4()
-
+        if self.wkt.startswith('GEOGCS'):
+            return True
+        return False
