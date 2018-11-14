5da90d19d074fee7e6ad45b764acca4c058ff565
geospatial-jeff <jeff.albrecht@slingshotaerospace.com>
2018-11-14 10:24:06 -0700

    updating setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+#Dont install pygdal_utils
+with open('./requirements.txt') as reqs:
+    requirements = [line.rstrip() for line in reqs if 'git' not in line]
+
+setup(name='gdaljson',
+      version='0.1',
+      description='JSON wrapper of the GDAL VRT spec',
+      author='Jeff Albrecht',
+      author_email='geospatialjeff@gmail.com',
+      packages=find_packages(exclude=["tests"]),
+      install_requires = requirements
+      )
