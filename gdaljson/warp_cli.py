6b260fa301e6f2787b509cc0ed6afee836ee5eaf
geospatial-jeff <jeff.albrecht@slingshotaerospace.com>
2018-11-15 08:13:39 -0700

    Adding cutline option to warp cli

--- gdaljson/warp_cli.py
+++ gdaljson/warp_cli.py
@@ -3,38 +3,43 @@ from gdaljson import VRTWarpedDataset
 
 
 @click.command()
-@click.argument("infile", type=click.File("r"))
-@click.argument("outfile", type=click.File("wb"))
-@click.option("--dstsrs", type=int)
+@click.argument("infile", type=click.File("r"), help="Input .VRT file")
+@click.argument("outfile", type=click.File("wb"), help="Output .VRT file")
+@click.option("--dstsrs", type=int, help="Output EPSG")
+@click.option(
+    "--cutline",
+    type=click.Path(exists=True)
+    help="Geojson file containing geojson feature in same SRS as raster"
+              ),
 @click.option(
     "--croptocutline",
-    help="Crop the output raster's extent to that of the cutline (boolean)",
+    help="Crop the output raster's extent to that of the cutline",
     type=bool,
 )
 @click.option(
     "--height",
-    help="Override height of output raster in # of pixels (int)",
+    help="Override height of output raster in # of pixels",
     type=int)
 @click.option(
     "--width",
-    help="Override width of output raster in # of pixels (int)",
+    help="Override width of output raster in # of pixels",
     type=int)
 @click.option(
     "--xres",
-    help="Override x-resolution of output raster (float)",
+    help="Override x-resolution of output raster",
     type=(float, int))
 @click.option(
     "--yres",
-    help="Override y-resolution of output raster (float)",
+    help="Override y-resolution of output raster",
     type=(float, int))
 @click.option(
     "--dstalpha",
-    help="Add an alpha band to the output raster (boolean",
+    help="Add an alpha band to the output raster",
     type=bool)
 @click.option(
     "--resample",
     default="NearestNeighbor",
-    help="Choose a resampling algorithm (string)",
+    help="Choose a resampling algorithm",
     type=str,
 )
 def cli(
@@ -52,6 +57,7 @@ def cli(
     vrt = VRTWarpedDataset(infile.read())
     vrt.warp(
         dstSRS=dstsrs,
+        clipper=cutline,
         cropToCutline=croptocutline,
         height=height,
         width=width,
